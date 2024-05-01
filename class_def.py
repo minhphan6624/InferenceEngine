ed8fa25ee354be205719f3e1b7b1d2bca49b498d
minhphan6624 <quangminh6624@gmail.com>
2024-05-01 21:49:04 +1000

    initial work on class definitions

---
+++ class_def.py
@@ -0,0 +1,194 @@
+from utils import *
+
+# Abstract knowledge base class
+
+
+class KB:
+    def __init__(self, sentence=None):
+        if sentence:
+            self.tell(sentence)
+
+    def tell(self, sentence):
+        # Add a sentence to the KB
+        raise NotImplementedError
+
+    def ask(self, query):
+        """Return a substitution that makes the query true, or, failing that, return False."""
+        return first(self.ask_generator(query), default=False)
+
+    def ask_generator(self, query):
+        """Yield all the substitutions that make query true."""
+        raise NotImplementedError
+
+    def retract(self, sentence):
+        """Remove sentence from the KB."""
+        raise NotImplementedError
+
+
+class PropKB(KB):
+    """A KB for propositional logic. Inefficient, with no indexing."""
+
+    def __init__(self, sentence=None):
+        super().__init__(sentence)
+        self.clauses = []
+
+    def tell(self, sentence):
+        """Add the sentence's clauses to the KB."""
+        self.clauses.extend(conjuncts(to_cnf(sentence)))
+
+    def ask_generator(self, query):
+        """Yield the empty substitution {} if KB entails query; else no results."""
+        if tt_entails(Expr('&', *self.clauses), query):
+            yield {}
+
+    def ask_if_true(self, query):
+        """Return True if the KB entails query, else return False."""
+        for _ in self.ask_generator(query):
+            return True
+        return False
+
+    def retract(self, sentence):
+        """Remove the sentence's clauses from the KB."""
+        for c in conjuncts(to_cnf(sentence)):
+            if c in self.clauses:
+                self.clauses.remove(c)
+
+
+class Expr:
+    """A mathematical expression with an operator and 0 or more arguments.
+    op is a str like '+' or 'sin'; args are Expressions.
+    Expr('x') or Symbol('x') creates a symbol (a nullary Expr).
+    Expr('-', x) creates a unary; Expr('+', x, 1) creates a binary."""
+
+    def __init__(self, op, *args):
+        self.op = str(op)
+        self.args = args
+
+    # Operator overloads
+    def __neg__(self):
+        return Expr('-', self)
+
+    def __pos__(self):
+        return Expr('+', self)
+
+    def __invert__(self):
+        return Expr('~', self)
+
+    def __add__(self, rhs):
+        return Expr('+', self, rhs)
+
+    def __sub__(self, rhs):
+        return Expr('-', self, rhs)
+
+    def __mul__(self, rhs):
+        return Expr('*', self, rhs)
+
+    def __pow__(self, rhs):
+        return Expr('**', self, rhs)
+
+    def __mod__(self, rhs):
+        return Expr('%', self, rhs)
+
+    def __and__(self, rhs):
+        return Expr('&', self, rhs)
+
+    def __xor__(self, rhs):
+        return Expr('^', self, rhs)
+
+    def __rshift__(self, rhs):
+        return Expr('>>', self, rhs)
+
+    def __lshift__(self, rhs):
+        return Expr('<<', self, rhs)
+
+    def __truediv__(self, rhs):
+        return Expr('/', self, rhs)
+
+    def __floordiv__(self, rhs):
+        return Expr('//', self, rhs)
+
+    def __matmul__(self, rhs):
+        return Expr('@', self, rhs)
+
+    def __or__(self, rhs):
+        """Allow both P | Q, and P |'==>'| Q."""
+        if isinstance(rhs, Expression):
+            return Expr('|', self, rhs)
+        else:
+            return PartialExpr(rhs, self)
+
+    # Reverse operator overloads
+    def __radd__(self, lhs):
+        return Expr('+', lhs, self)
+
+    def __rsub__(self, lhs):
+        return Expr('-', lhs, self)
+
+    def __rmul__(self, lhs):
+        return Expr('*', lhs, self)
+
+    def __rdiv__(self, lhs):
+        return Expr('/', lhs, self)
+
+    def __rpow__(self, lhs):
+        return Expr('**', lhs, self)
+
+    def __rmod__(self, lhs):
+        return Expr('%', lhs, self)
+
+    def __rand__(self, lhs):
+        return Expr('&', lhs, self)
+
+    def __rxor__(self, lhs):
+        return Expr('^', lhs, self)
+
+    def __ror__(self, lhs):
+        return Expr('|', lhs, self)
+
+    def __rrshift__(self, lhs):
+        return Expr('>>', lhs, self)
+
+    def __rlshift__(self, lhs):
+        return Expr('<<', lhs, self)
+
+    def __rtruediv__(self, lhs):
+        return Expr('/', lhs, self)
+
+    def __rfloordiv__(self, lhs):
+        return Expr('//', lhs, self)
+
+    def __rmatmul__(self, lhs):
+        return Expr('@', lhs, self)
+
+    def __call__(self, *args):
+        """Call: if 'f' is a Symbol, then f(0) == Expr('f', 0)."""
+        if self.args:
+            raise ValueError('Can only do a call for a Symbol, not an Expr')
+        else:
+            return Expr(self.op, *args)
+
+    # Equality and repr
+    def __eq__(self, other):
+        """x == y' evaluates to True or False; does not build an Expr."""
+        return isinstance(other, Expr) and self.op == other.op and self.args == other.args
+
+    def __lt__(self, other):
+        return isinstance(other, Expr) and str(self) < str(other)
+
+    def __hash__(self):
+        return hash(self.op) ^ hash(self.args)
+
+    def __repr__(self):
+        op = self.op
+        args = [str(arg) for arg in self.args]
+        if op.isidentifier():  # f(x) or f(x, y)
+            return '{}({})'.format(op, ', '.join(args)) if args else op
+        elif len(args) == 1:  # -x or -(x + 1)
+            return op + args[0]
+        else:  # (x - y)
+            opp = (' ' + op + ' ')
+            return '(' + opp.join(args) + ')'
+
+
+# An 'Expression' is either an Expr or a Number.
+# Symbol is not an explicit type; it is any Expr with 0 args.
---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+filename = sys.argv[1]
+method = sys.argv[2]
+
+
+def parse_input_file(filename):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
