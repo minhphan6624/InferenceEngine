ba8e9ce4eba6af7350e6264b1a9fca0c7b903b8b
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2024-05-02 11:03:18 +1000

    restructure the files + parsing input

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
+from KB import *
 
 filename = sys.argv[1]
-method = sys.argv[2]
-
+#method = sys.argv[2]
 
 def parse_input_file(filename):
-    pass
+    with open(filename, "r") as f:
+        f.readline() #First line only contains "TELL"
+        tell_sentences = f.readline().strip().split(";")
+
+        for sentence in tell_sentences:
+            sentence = sentence.replace(" ", "'") #Remove space inside a sentence
+            if (sentence.find("=>")):
+                
 
 
 def main():
